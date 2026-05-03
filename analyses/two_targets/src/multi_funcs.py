7ea3210b139d6916d43652ee13cc5464371d2821
jchen80 <jychen835@gmail.com>
2026-05-02 21:44:50 -0400

    add docstring

--- analyses/two_targets/src/multi_funcs.py
+++ analyses/two_targets/src/multi_funcs.py
@@ -2,6 +2,13 @@ import libs.utils as util
 
 # Transform data
 def add_pairwise_metrics(trk_, feat_, calib, flyid1=0, flyid2=1):
+    """
+    Adds the following pairwise metrics to feat_:
+    - dist_to_other: distance to the other fly
+    - facing_angle: angle between fly's orientation and the line connecting the two flies
+
+    Assumes that trk_ and feat_ are already filtered to only contain the two fly ids of interest (flyid1 and flyid2).
+    """
     ppm = calib.get('PPM', 1)
     for fid, oid in [(flyid1, flyid2), (flyid2, flyid1)]:
         f_trk = trk_[trk_['id']==fid]
